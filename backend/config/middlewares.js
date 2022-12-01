d0fc6ab9010a7952cd0849fc7d63ec2c455c83ba
sushant-metaversity <sushant@metaversitytechnologies.com>
2022-12-01 12:19:39 +0530

    Add csp security

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,6 +1,27 @@
-module.exports = [
+module.exports =({env}) => [
   'strapi::errors',
-  'strapi::security',
+  // 'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            'dl.airtable.com',
+            "metaversity-technologies.s3.ap-south-1.amazonaws.com",
+          ],
+          "media-src": ["'self'", "data:", "blob:",
+          `https://${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
